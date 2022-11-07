1f366b962d3378fc0bfdc3e2afa1f6279f1bf7a2
HeOleg <lu4shemeb@gmao;.com>
2022-11-07 18:01:49 +0300

    Сделал Задание 3.

--- src/Car.java
+++ src/Car.java
@@ -19,11 +19,56 @@ public class Car {
 
     @Override
     public String toString() {
-        return "Бренд: " + brand +
-                ". Модель: " + model +
-                ". Объем двигателя: " + engineCapacity +
-                ". Цвет кузова: " + bodyColor +
-                ". Год выпуска: " + productionYear +
-                " Страна выпуска: " + assemblyCountry + ".";
+        return "Бренд: " + getBrand() +
+                ". Модель: " + getModel() +
+                ". Объем двигателя: " + getEngineCapacity()+
+                ". Цвет кузова: " + getBodyColor() +
+                ". Год выпуска: " + getProductionYear() +
+                " Страна выпуска: " + getProductionYear() + ".";
+    }
+
+    public String getBrand() {
+        if (brand != null && !brand.isEmpty() && !brand.isBlank()) {
+            return this.brand;
+        } else {
+            return "default";
+        }
+    }
+
+    public String getModel() {
+            if (model != null && !model.isEmpty() && !model.isBlank()) {
+                return this.model;
+            } else {
+                return "default";
+            }
+        }
+
+    public double getEngineCapacity() {
+        if (engineCapacity != 0) {
+            return this.engineCapacity;
+        } else {
+            return 1.5;
+        }
+    }
+
+    public String getBodyColor() {
+        return bodyColor;
+    }
+
+    public int getProductionYear() {
+        if (productionYear != 0) {
+            return productionYear;
+        } else {
+            return 2000;
+        }
+    }
+
+    public String getAssemblyCountry() {
+        if ( assemblyCountry != null && !assemblyCountry.isEmpty() && !assemblyCountry.isBlank()){
+            return this.assemblyCountry;
+        }
+        else {
+            return "default";
+        }
     }
 }
